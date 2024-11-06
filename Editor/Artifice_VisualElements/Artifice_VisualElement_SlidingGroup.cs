df2f839dd25d15fb2b8054345a874e72fd970c79
zackper <pervolarakis@abzorbagames.com>
2024-11-06 13:20:21 +0200

    Added changes to support throughout Uniyt 2021 to 2023 regarding sliding group and minor obsolute method

--- Editor/Artifice_VisualElements/Artifice_VisualElement_SlidingGroup.cs
+++ Editor/Artifice_VisualElements/Artifice_VisualElement_SlidingGroup.cs
@@ -1,5 +1,6 @@
 using ArtificeToolkit.Editor;
 using ArtificeToolkit.Editor.Artifice_CustomAttributeDrawers.CustomAttributeDrawers_Groups;
+using UnityEngine;
 using UnityEngine.UIElements;
 using UnityEngine.UIElements.Experimental;
 
@@ -14,16 +15,15 @@ namespace Editor.Artifice_VisualElements
         
         private Button _handeElem;
 
-        private ValueAnimation<float> _valueAnimation = new ValueAnimation<float>();
+        private ValueAnimation<float> _valueAnimation = new();
+
+        private const int ToggleAnimationDurationMillis = 350;
         
         public Artifice_VisualElement_SlidingGroup() : base()
         {
             styleSheets.Add(Artifice_Utilities.GetStyle(GetType()));
-            IsExpanded = false;
-            SlideOff();
             
             // Default content is created in base constructor.
-            // Make default container's display None at beginning to not affect the layout composition. Otherwise it seems to enlarge the container of the inspector.
             DefaultContentContainer.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.None);
             
             // Create handle button
@@ -31,6 +31,10 @@ namespace Editor.Artifice_VisualElements
             _handeElem.text = "Sliding Group";
             _handeElem.AddToClassList("handle");
             hierarchy.Insert(0, _handeElem);
+            
+            // Make default state be slided off for now.
+            IsExpanded = false;
+            SlideOff();
         }
 
         public override void SetTitle(string title)
@@ -50,38 +54,60 @@ namespace Editor.Artifice_VisualElements
 
         private void SlideOn()
         {
-            // Stop potential previous animation
             if(_valueAnimation != null && _valueAnimation.isRunning)
-                _valueAnimation?.Stop();
+                _valueAnimation.Stop();
             
             // Make visible and flex.
             DefaultContentContainer.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.Flex);
             DefaultContentContainer.style.position = new StyleEnum<Position>(Position.Absolute);
             
+            #if UNITY_2022_1_OR_NEWER
+            
             // Begin animation!
             var width = resolvedStyle.width;
-            _valueAnimation = DefaultContentContainer.experimental.animation.Start(1f, 0f, 400, (elem, value) =>
+            _valueAnimation = DefaultContentContainer.experimental.animation.Start(1f, 0f, ToggleAnimationDurationMillis, (elem, value) =>
             {
-                elem.style.translate = new StyleTranslate(new Translate(width * value, 0f));
+                elem.style.translate = new StyleTranslate(new Translate(value * width, 0f));
             });
+            
+            #else
+            
+            // Begin animation!
+            var width = resolvedStyle.width;
+            _valueAnimation = DefaultContentContainer.experimental.animation.Start(0f, 1f, ToggleAnimationDurationMillis, (elem, value) =>
+            {
+                elem.style.scale = new StyleScale(new Scale(new Vector2(value, value)));
+            });
+            
+            #endif
         }
 
         private void SlideOff()
         {
-            // Stop potential previous animation
             if(_valueAnimation != null && _valueAnimation.isRunning)
-                _valueAnimation?.Stop();
+                _valueAnimation.Stop();
             
-            // Make visible and flex.
-            DefaultContentContainer.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.Flex);
-            DefaultContentContainer.style.position = new StyleEnum<Position>(Position.Absolute);
+#if UNITY_2022_1_OR_NEWER
             
             // Begin animation!
             var width = resolvedStyle.width;
-            _valueAnimation = DefaultContentContainer.experimental.animation.Start(0f, 1f, 400, (elem, value) =>
+            _valueAnimation = DefaultContentContainer.experimental.animation.Start(0f, 1f, ToggleAnimationDurationMillis, (elem, value) =>
             {
-                elem.style.translate = new StyleTranslate(new Translate(width * value, 0f));
-            });
+                elem.style.translate = new StyleTranslate(new Translate(value * width, 0f));
+            })
+                .OnCompleted(() => DefaultContentContainer.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.None));;
+            
+#else
+            
+            // Begin animation!
+            var width = resolvedStyle.width;
+            _valueAnimation = DefaultContentContainer.experimental.animation.Start(1f, 0f, ToggleAnimationDurationMillis, (elem, value) =>
+            {
+                elem.style.scale = new StyleScale(new Scale(new Vector2(value, value)));
+            })
+                .OnCompleted(() => DefaultContentContainer.style.display = new StyleEnum<DisplayStyle>(DisplayStyle.None));
+            
+#endif
         }
     }
 }
